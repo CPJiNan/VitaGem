830d4f42c4347aca8138ce107b615095772d82c4
季楠 <2759278070@qq.com>
2025-05-31 11:33:08 +0800

    [2.0.0] [+]新增若干内容

--- src/main/kotlin/com/github/cpjinan/plugin/vitagem/command/MainCommand.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/vitagem/command/MainCommand.kt
@@ -3,6 +3,7 @@ package com.github.cpjinan.plugin.vitagem.command
 import com.github.cpjinan.plugin.vitagem.VitaGem
 import com.github.cpjinan.plugin.vitagem.VitaGemSettings
 import com.github.cpjinan.plugin.vitagem.event.PluginReloadEvent
+import com.github.cpjinan.plugin.vitagem.utils.LoggerUtils.debug
 import taboolib.common.platform.ProxyCommandSender
 import taboolib.common.platform.command.*
 import taboolib.common.util.isConsole
@@ -56,15 +57,26 @@ object MainCommand {
     )
     val reload = subCommand {
         execute { sender: ProxyCommandSender, _: CommandContext<ProxyCommandSender>, _: String ->
+            debug("&8[&3Vita&bGem&8] &5调试&7#1 &8| &6触发插件重载命令，正在展示处理逻辑。")
+
             PluginReloadEvent.Pre().call()
+            val start = System.currentTimeMillis()
+            var time = start
 
             VitaGemSettings.settings.reload()
+            debug("&r| &b◈ &r#1 配置文件重载完成，用时 ${System.currentTimeMillis() - time}ms。")
+            time = System.currentTimeMillis()
 
             val languageAPI = VitaGem.api().getLanguage()
             languageAPI.reload()
+            debug("&r| &b◈ &r#1 语言文件重载完成，用时 ${System.currentTimeMillis() - time}ms。")
+            time = System.currentTimeMillis()
 
             val serviceAPI = VitaGem.api().getService()
             serviceAPI.reload()
+            debug("&r| &b◈ &r#1 业务逻辑重载完成，共加载 ${serviceAPI.gemConfigDataMap.size} 个宝石、${serviceAPI.tableConfigDataMap.size} 个界面，用时 ${System.currentTimeMillis() - time}ms。")
+
+            debug("&r| &a◈ &r#1 插件重载完毕，总计用时 ${System.currentTimeMillis() - start}ms。")
 
             PluginReloadEvent.Post().call()
             sender.sendLang("Plugin-Reloaded")
