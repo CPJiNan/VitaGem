c4203efeb728fe92a1c05c54832857f0b0d0a851
季楠 <2759278070@qq.com>
2025-06-02 20:17:00 +0800

    [2.0.0] [+]新增若干内容

--- src/main/kotlin/com/github/cpjinan/plugin/vitagem/listener/PlayerListener.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/vitagem/listener/PlayerListener.kt
@@ -15,14 +15,22 @@ import taboolib.common.platform.event.SubscribeEvent
 object PlayerListener {
     @SubscribeEvent
     fun onPlayerSocket(event: PlayerSocketEvent) {
+        val result = event.result.toMutableMap()
+        result.remove("Data")
+        result.remove("Item.ItemStack")
+        result.remove("Gem.ItemStack")
         debug("&8[&3Vita&bGem&8] &5调试&7#2 &8| &6触发玩家镶嵌事件，正在展示处理逻辑。")
         debug("&r| &b◈ &r#2 玩家名称: ${event.player.name}。")
-        debug("&r| &b◈ &r#2 镶嵌结果: ${event.result}。")
+        debug("&r| &b◈ &r#2 镶嵌结果: ${result}。")
         debug("&r| &b◈ &r#2 事件处理完毕。")
     }
 
     @SubscribeEvent
     fun onPlayerExtract(event: PlayerExtractEvent) {
+        val result = event.result.toMutableMap()
+        result.remove("Data")
+        result.remove("Item.ItemStack")
+        result.remove("Gem.ItemStack")
         debug("&8[&3Vita&bGem&8] &5调试&7#3 &8| &6触发玩家拆卸事件，正在展示处理逻辑。")
         debug("&r| &b◈ &r#3 玩家名称: ${event.player.name}。")
         debug("&r| &b◈ &r#3 拆卸结果: ${event.result}。")
